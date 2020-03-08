9698b836b563ac899f43c76c434c40c9ac486e27
Taylor Oliveira <taylorsantosoliveira@hotmail.com>
2020-03-08 02:04:04 -0300

    pagnation prev next

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -14,25 +14,38 @@ export default class Main extends Component {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
     const { docs, ...productsInfo } = response.data;
 
-    this.setState({products: docs, productsInfo});
+    this.setState({products: docs, productsInfo, page});
   };
 
   prevPage = () => {
+    const { page, productsInfo } = this.state;
 
-  }
+    if(page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+
+  };
 
   nextPage = () => {
+    const { page, productsInfo } = this.state;
 
-  }
+    if(page === productsInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  };
 
   render() {
 
-    const { products } = this.state;
+    const { products, page, productsInfo } = this.state;
 
     return (
       <div className="product-list">
@@ -44,8 +57,8 @@ export default class Main extends Component {
           </article>
         ))}
         <div className="actions">
-          <button onClick={this.prevPage}>Anterior</button>
-          <button onClick={this.nextPage}>Próximo</button>
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productsInfo.pages} onClick={this.nextPage}>Próximo</button>
         </div>
       </div>
     )
